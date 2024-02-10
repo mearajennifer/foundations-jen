5cbfbaa91789bbf1a95b2c4ec2670582de87a6d0
mearajennifer <jenniferlmeara@gmail.com>
2024-02-10 09:16:47 -0500

    unit 1

---
+++ 02-running-code/myfile.js
@@ -0,0 +1,4 @@
+console.log("Hello world");
+
+let message = "Hello again, world";
+console.log(message);
---
+++ daily-challenges.js
@@ -0,0 +1,25 @@
+///// W1D3: Max of Two
+// let x = 2;
+// let y = 2;
+
+// if (x > y) {
+//     console.log(x);
+// } else if (y > x) {
+//     console.log(y);
+// } else {
+//     console.log("The values are identical");
+// }
+
+///// W1D4: Fizz Buzz
+// for (i = 1; i < 51; i++) {
+//     if (i % 5 === 0 && i % 3 === 0) {
+//         console.log("fizzbuzz");
+//     } else if (i % 5 === 0) {
+//         console.log("buzz");
+//     } else if (i % 3 === 0) {
+//         console.log("fizz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
